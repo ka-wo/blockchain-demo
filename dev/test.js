95c7579d69ffd08afa992d2555f902f58a3e42cb
Kacper Wolkowski <kacper.wolkowski@gmail.com>
2021-09-30 13:06:15 +0200

    Test creation of new blocks

---
+++ dev/test.js
@@ -0,0 +1,13 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2389, 'OD8SD2723', '123DSA2D2R');
+bitcoin.createNewBlock(111, 'AS8DS7D72D', 'D2D22D2D222334');
+bitcoin.createNewBlock(2151, 'DA2D2D2D22DR43', '55161SCSC');
+
+
+console.log(bitcoin);
+
+
+
