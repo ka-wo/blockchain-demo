3c1274aa5cdc22ccbedd631fc0406253683956a3
Kacper Wolkowski <kacper.wolkowski@gmail.com>
2021-09-30 23:14:11 +0200

    Add creation of new transaction

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,19 +1,19 @@
 function Blockchain() {
     this.chain = []; // all of the blocks that we create and mine will be store here
-    this.newTransactions = []; // all of the new transactions that will be created before they are placed into a block
+    this.pendingTransactions = []; // all of the new transactions that will be created before they are placed into a block
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
      const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce, //proof of work, just a number in this case
         hash: hash,
         previousBlockHash: previousBlockHash
      };
 
-     this.newTransactions = []; // The block contains all the previous transactions so we have to empty the new ones
+     this.pendingTransactions = []; // The block contains all the previous transactions so we have to empty the new ones
      this.chain.push(newBlock);
 
      return newBlock
@@ -21,6 +21,18 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
 
 Blockchain.prototype.getLastBlock = function() {
     return this.chain[this.chain.length-1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    };
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1; // we want to return an index of a block that will hold this transaction - it will be the one created after creating this transaction (that's why we have +1)
 }
 
 module.exports = Blockchain;
