8e65678196151dfa3fd285f31606d2f689a1bd07
Kacper Wolkowski <kacper.wolkowski@gmail.com>
2021-10-02 23:18:00 +0200

    Add genesis block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,6 +3,9 @@ const sha256 = require('sha256');
 function Blockchain() {
     this.chain = []; // all of the blocks that we create and mine will be store here
     this.pendingTransactions = []; // all of the new transactions that will be created before they are placed into a block
+
+    // generate a genesis block - the very first one
+    this.createNewBlock(100, '0', '0');
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
