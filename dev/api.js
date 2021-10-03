6fe8ee516a8723acab8b08ce653964f487a5a087
Kacper Wolkowski <kacper.wolkowski@gmail.com>
2021-10-03 21:49:31 +0200

    Add skelet of api endpoints

---
+++ dev/api.js
@@ -0,0 +1,21 @@
+var express = require('express');
+const Blockchain = require('./blockchain');
+var app = express();
+
+
+app.get('/blockchain', function (req, res) {
+
+});
+
+app.post('/transaction', function(req, res){
+
+});
+
+app.get('/mine', function(req, res){
+
+});
+
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000...');
+});
