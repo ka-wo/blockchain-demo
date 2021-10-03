9ee5f901561db92a2c17ffb701f281641ffda95c
Kacper Wolkowski <kacper.wolkowski@gmail.com>
2021-10-03 22:37:43 +0200

    Add body parser and test /transaction endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,6 +1,10 @@
 var express = require('express');
 const Blockchain = require('./blockchain');
 var app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/blockchain', function (req, res) {
@@ -8,7 +12,8 @@ app.get('/blockchain', function (req, res) {
 });
 
 app.post('/transaction', function(req, res){
-
+    console.log(req.body);
+    res.send(`The amount of the transaction is ${req.body.amount} bitcoin`)
 });
 
 app.get('/mine', function(req, res){
